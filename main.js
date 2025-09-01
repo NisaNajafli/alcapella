9b98f03ad127f4274990350f7e79ada4ff8d63c7
NisaNajafli <ncfliniss@gmail.com>
2025-09-01 17:29:50 +0400

    new

--- main.js
+++ main.js
@@ -3,4 +3,14 @@ faqs.forEach( faq => {
     faq.addEventListener("click", () => {
         faq.classList.toggle("active");
     })
-})
+});
+
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("nav");
+  if (window.scrollY > 0) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
